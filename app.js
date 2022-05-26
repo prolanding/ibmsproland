71eb5062e5a9807a8407dfd0f58c6dbe97e819c5
ibmsproland <ibmsproland@gmail.com>
2022-05-26 19:36:47 +0530

    setting up the login and signup page

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+const validationRouter = express.Router();
+
+app.set('view engine','ejs');
+app.use(express.static('./public'));
+app.set('views','./src/views');
 
 app.get("/",(req,res)=>{
- res.send('hello world');
+ res.render('index');
 });
+app.get("/login",(req,res)=>{
+ res.render('login');
+});
+app.get("/signup",(req,res)=>{
+ res.render('signup');
+});
+app.use("/verification",validationRouter);
 app.listen(2800)
